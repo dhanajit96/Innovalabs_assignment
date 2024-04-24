f11e31693c8c3df6002be2ae38775fe2fbbb4eb3
dhanajit96 <ajitdhanje@gmail.com>
2024-04-24 09:44:16 +0530

    added the final fix

--- app/controllers/api/blogs_controller.rb
+++ app/controllers/api/blogs_controller.rb
@@ -1,6 +1,5 @@
 class Api::BlogsController < ApplicationController
   before_action :verify_token
-  #   skip_before_action :require_login, only: [:index]
 
   def index
     render json: Blog.all
@@ -17,7 +16,7 @@ class Api::BlogsController < ApplicationController
   def valid_token?
     token = request.headers["Authorization"]&.split(" ")&.last
     return false if token.nil?
-    user = User.find_by(auth_token: token).present?
+    user = User.find_by(auth_token: token)
     return true if user && user.valid_till > Time.now
   end
 end
